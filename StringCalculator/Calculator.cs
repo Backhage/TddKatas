5388345fc6cbeda059b4a590427540e7cea7d73a
Eric Backhage <eric.backhage@gmail.com>
2018-03-09 17:00:13 +0100

    Added String Calculator TDD Kata

---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        private const string SeparatorIdentifier = "//";
+
+        public static int Add(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return 0;
+            }
+
+            var separators = new List<char> { ',', '\n' };
+            if (ContainsUserDefinedSeparator(input))
+            {
+                separators.Add(ExtractUserDefinedSeparator(input));
+                input = RemoveSeparatorInfo(input);
+            }
+
+            IEnumerable<int> values = ExtractValues(input, separators);
+            Validate(values);
+
+            return values.Sum();
+        }
+
+        private static bool ContainsUserDefinedSeparator(string input)
+        {
+            return input.StartsWith(SeparatorIdentifier);
+        }
+
+        private static char ExtractUserDefinedSeparator(string input)
+        {
+            return input[SeparatorIdentifier.Length];
+        }
+
+        private static string RemoveSeparatorInfo(string input)
+        {
+            return input.Substring($"{SeparatorIdentifier}.\n".Length);
+        }
+
+        private static IEnumerable<int> ExtractValues(string input, List<char> separators)
+        {
+            return input
+                .Split(separators.ToArray())
+                .Select(s => int.Parse(s));
+        }
+
+        private static void Validate(IEnumerable<int> values)
+        {
+            var negatives = values.Where(n => n < 0);
+            if (negatives.Any())
+            {
+                throw new ArgumentException($"Negatives not allowed: {negatives.ToArray()}");
+            }
+        }
+    }
+}
---
+++ StringCalculatorTest/CalculationTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using StringCalculator;
+using System;
+
+namespace StringCalculatorTest
+{
+    [TestFixture]
+    public class CalculationTests
+    {
+        public void TestAdd()
+        {
+            Assert.AreEqual(0, Calculator.Add(""));
+            Assert.AreEqual(1, Calculator.Add("1"));
+            Assert.AreEqual(10, Calculator.Add("1,2,3,4"));
+        }
+
+        [Test]
+        public void TestDifferentSeparators()
+        {
+            Assert.AreEqual(6, Calculator.Add("1\n2,3"));
+        }
+
+        [Test]
+        public void TestChangingSeparator()
+        {
+            Assert.AreEqual(7, Calculator.Add("//;\n3;4"));
+        }
+
+        [Test]
+        public void TestNegative()
+        {
+            Assert.Throws<ArgumentException>(() => Calculator.Add("-1"));
+            Assert.Throws<ArgumentException>(() => Calculator.Add("1,-1"));
+        }
+    }
+}
