bc72549c2622ab476f0b51ee4164e43a65ce8fdc
Eric Backhage <eric.backhage@gmail.com>
2018-03-10 17:23:51 +0100

    Added Prime Factors TDD Kata (Original author Uncle Bob Martin)

---
+++ PrimeFactors/PrimeFactors.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace PrimeFactors
+{
+    public static class PrimeFactors
+    {
+        public static List<int> Generate(int n)
+        {
+            var primes = new List<int>();
+            for (var candidate = 2;  n > 1; candidate++)
+            {
+                for (; n % candidate == 0; n /= candidate)
+                {
+                    primes.Add(candidate);
+                }
+            }
+            return primes;
+        }
+    }
+}
---
+++ PrimeFactorsTest/PrimeFactorsTest.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using static PrimeFactors.PrimeFactors;
+
+namespace PrimeFactorsTest
+{
+    [TestFixture]
+    public class PrimeFactorsTest
+    {
+        private List<int> List(params int[] ints)
+        {
+            var list = new List<int>();
+            list.AddRange(ints);
+            return list;
+        }
+
+        [Test]
+        public void TestOne()
+        {
+            Assert.AreEqual(List(), Generate(1));
+        }
+
+        [Test]
+        public void TestTwo()
+        {
+            Assert.AreEqual(List(2), Generate(2));
+        }
+
+        [Test]
+        public void TestThree()
+        {
+            Assert.AreEqual(List(3), Generate(3));
+        }
+
+        [Test]
+        public void TestFour()
+        {
+            Assert.AreEqual(List(2,2), Generate(4));
+        }
+
+        [Test]
+        public void TestSix()
+        {
+            Assert.AreEqual(List(2,3), Generate(6));
+        }
+
+        [Test]
+        public void TestEight()
+        {
+            Assert.AreEqual(List(2,2,2), Generate(8));
+        }
+
+        [Test]
+        public void TestNine()
+        {
+            Assert.AreEqual(List(3,3), Generate(9));
+        }
+    }
+}
