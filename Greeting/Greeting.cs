b06f819548794679f0f6a5def0347f1f2f7a571e
Eric Backhage <eric.backhage@gmail.com>
2018-03-12 22:57:21 +0100

    Add Greeting Kata

---
+++ Greeting/Greeting.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Greeting
+{
+    public static class Greeting
+    {
+        public static string Greet(params string[] names)
+        {
+            if (names == null)
+            {
+                return DefaultGreeting();
+            }
+            return GenerateGreeting(names);
+        }
+
+        private static string DefaultGreeting()
+        {
+            return "Hello, my friend.";
+        }
+
+        private static string GenerateGreeting(string[] names)
+        {
+            var upperCaseNames = names.Where(s => s.All(char.IsUpper)).ToList();
+            var regularCaseNames = names.Except(upperCaseNames).ToList();
+            var greeting = new StringBuilder();
+
+            AddNormalCaseGreeting(regularCaseNames, greeting);
+
+            if (regularCaseNames.Any() && upperCaseNames.Any())
+            {
+                greeting.Append(" AND ");
+            }
+
+            AddUpperCaseGreeting(upperCaseNames, greeting);
+
+            return greeting.ToString();
+        }
+
+        private static void AddNormalCaseGreeting(List<string> names, StringBuilder greeting)
+        {
+            if (names.Count == 0) return;
+
+            greeting.Append("Hello, ");
+            greeting.Append(names.First());
+
+            if (names.Count > 2)
+            {
+                AddCommaSeparated(names, greeting);
+            }
+
+            if (names.Count > 1)
+            {
+                greeting.Append(" and ");
+                greeting.Append(names.Last());
+            }
+
+            greeting.Append(".");
+        }
+
+        private static void AddUpperCaseGreeting(List<string> names, StringBuilder greeting)
+        {
+            if (names.Count == 0) return;
+
+            greeting.Append("HELLO ");
+            greeting.Append(names.First());
+
+            if (names.Count > 2)
+            {
+                AddCommaSeparated(names, greeting);
+            }
+
+            if (names.Count > 1)
+            {
+                greeting.Append(" AND ");
+                greeting.Append(names.Last());
+            }
+
+            greeting.Append("!");
+        }
+
+        private static void AddCommaSeparated(List<string> names, StringBuilder greeting)
+        {
+            greeting.Append(",");
+            for (var nameIndex = 1; nameIndex < names.Count - 1; nameIndex++)
+            {
+                greeting.Append(" ");
+                greeting.Append(names[nameIndex]);
+                greeting.Append(",");
+            }
+        }
+    }
+}
---
+++ GreetingTest/GreetingTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using static Greeting.Greeting;
+
+namespace GreetingTest
+{
+    [TestFixture]
+    [Category("Unit")]
+    public class GreetingTests
+    {
+        [Test]
+        public void TestGreet()
+        {
+            Assert.AreEqual("Hello, Bob.", Greet("Bob"));
+        }
+
+        [Test]
+        public void TestNull()
+        {
+            Assert.AreEqual("Hello, my friend.", Greet(null));
+        }
+
+        [Test]
+        public void TestShouting()
+        {
+            Assert.AreEqual("HELLO JERRY!", Greet("JERRY"));
+        }
+
+        [Test]
+        public void TestMultipleNames()
+        {
+            Assert.AreEqual("Hello, Jill and Jane.", Greet("Jill", "Jane"));
+            Assert.AreEqual("Hello, Amy, Brian, and Charlotte.", Greet("Amy", "Brian", "Charlotte"));
+        }
+
+        [Test]
+        public void TestNormalAndShouting()
+        {
+            Assert.AreEqual("Hello, Jill. AND HELLO ANDREW!", Greet("Jill", "ANDREW"));
+            Assert.AreEqual("Hello, Jill and Jane. AND HELLO JACK AND JOE!", Greet("Jill", "Jane", "JACK", "JOE"));
+            Assert.AreEqual("Hello, Judy, Jane, and Jill. AND HELLO JIM, JACK, AND JOE!", Greet("Judy", "Jane", "JIM", "Jill", "JACK", "JOE"));
+        }
+    }
+}
